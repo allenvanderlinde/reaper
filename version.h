f5dbb3d33564cb43eff915972f0f5f0f9ee77a6e
Allen Vanderlinde <allen.vanderlinde@gmail.com>
2016-06-22 10:24:18 -0400

    Code-base Update
    Cleaned up code and continuing work on COutputResults. Thinking about
    using a struct which contains pertinent aspects of a HTML page (doctype,
    language, header, body, elements.

--- version.h
+++ version.h
@@ -4,7 +4,7 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char DATE[] = "20";
+	static const char DATE[] = "22";
 	static const char MONTH[] = "06";
 	static const char YEAR[] = "2016";
 	static const char UBUNTU_VERSION_STYLE[] =  "16.06";
@@ -16,17 +16,17 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR  = 1;
 	static const long MINOR  = 10;
-	static const long BUILD  = 23;
-	static const long REVISION  = 183;
+	static const long BUILD  = 24;
+	static const long REVISION  = 186;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 189;
-	#define RC_FILEVERSION 1,10,23,183
-	#define RC_FILEVERSION_STRING "1, 10, 23, 183\0"
-	static const char FULLVERSION_STRING [] = "1.10.23.183";
+	static const long BUILDS_COUNT  = 190;
+	#define RC_FILEVERSION 1,10,24,186
+	#define RC_FILEVERSION_STRING "1, 10, 24, 186\0"
+	static const char FULLVERSION_STRING [] = "1.10.24.186";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY  = 13;
+	static const long BUILD_HISTORY  = 14;
 	
 
 }
